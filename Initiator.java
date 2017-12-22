52f25c47ed65729fb81cbced1a00d8a87513599e
ayushkumar-1 <34773075+ayushkumar-1@users.noreply.github.com>
2017-12-23 00:40:58 +0530

    Add files via upload

---
+++ Initiator.java
@@ -0,0 +1,33 @@
+package com.test.swiggy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Initiator {
+	
+	//main
+	public static void main(String[] args) throws IOException {
+		
+		//Question 1
+		/**
+		 * Largest number (less than N[input]) with all digits as prime
+		 */
+		Question1 que1 = new Question1();
+
+		        List<Integer> li = new ArrayList<>();
+		        int i = 10;
+		        li.add(10);
+		        int j = li.get(0);
+		        System.out.println(j);
+		    
+		
+		//que1.PrintMaxPrimeDigitsNum();
+		
+		//que1.PrintMaxPrimeDigitsNumThreads();
+		
+	}
+
+}
---
+++ Question2.java
@@ -0,0 +1,5 @@
+package com.test.swiggy;
+
+public class Question2 {
+
+}
